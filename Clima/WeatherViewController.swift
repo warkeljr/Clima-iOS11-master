738bb6b3f917508ae8909ae93039b34d66af9c60
Wim van Arkel <warkel@me.com>
2017-11-27 23:06:42 +0100

    Pods installed en ready to go

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -13,7 +13,7 @@ class WeatherViewController: UIViewController {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "1f1921d0b33a95402eaad698a7e2f3e5"
     
 
     //TODO: Declare instance variables here
