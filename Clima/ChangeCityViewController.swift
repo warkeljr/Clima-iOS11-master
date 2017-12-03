31ef0bbf49964d2b86e4d1e5f98488f2c0b4204e
Wim van Arkel <warkel@me.com>
2017-12-03 09:56:46 +0100

    App is working fine, some fine tuning necessary

--- Clima/ChangeCityViewController.swift
+++ Clima/ChangeCityViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 
 //Write the protocol declaration here:
-
+protocol ChangeCityDelegate {
+    func userEnteredANewCityName(city: String)
+}
 
 
 class ChangeCityViewController: UIViewController {
     
     //Declare the delegate variable here:
+    var delegate : ChangeCityDelegate?
 
     
     //This is the pre-linked IBOutlets to the text field:
@@ -28,13 +31,14 @@ class ChangeCityViewController: UIViewController {
         
         
         //1 Get the city name the user entered in the text field
+        let cityName = changeCityTextField.text!
         
         
         //2 If we have a delegate set, call the method userEnteredANewCityName
-        
+        delegate?.userEnteredANewCityName(city: cityName)
         
         //3 dismiss the Change City View Controller to go back to the WeatherViewController
-        
+        dismiss(animated: true, completion: nil)
         
     }
     
